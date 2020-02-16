b78f7136ddb92fbdb31096523a8ef6a6e4c954ea
sbx_user1051 <sbx_user1051@169.254.19.173>
2020-02-16 19:45:24 +0000

    Sun Feb 16 19:45:24 UTC 2020

--- lib/solutions/CHK/checkout.py
+++ lib/solutions/CHK/checkout.py
@@ -21,10 +21,9 @@ class Checkout(object):
         offers = self.shop.get_offers()
 
         for offer in offers:
-            print(offer)
-            while offer[1]<= items[offer[0]]:
+            while offers[offer][1]<= items[offer[0]]:
                 total += items[2]
-                items[offer[0]] -= offer[1]
+                items[offer[0]] -= offers[offer][1]
 
         for sku in items:
             price = self.shop.get_price(sku)
