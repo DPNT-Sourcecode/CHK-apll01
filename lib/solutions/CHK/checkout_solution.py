621b2486a6477f65f20377a27846f6d2a8258bc3
sbx_user1051 <sbx_user1051@169.254.19.173>
2020-02-16 20:00:24 +0000

    Sun Feb 16 20:00:24 UTC 2020

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -17,6 +17,5 @@ def checkout(skus):
 
     for sku in skus:
         checkout.add_item(sku)
-
-    print(checkout.get_total())
+        
     return checkout.get_total()
--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -3,4 +3,8 @@ from solutions.CHK import checkout_solution
 
 class TestCheckout():
     def test_checkout(self):
-        assert checkout_solution.checkout('AAA') == 3
+        assert checkout_solution.checkout('AAA') == 130
+        assert checkout_solution.checkout('BB') == 45
+        assert checkout_solution.checkout('C') == 20
+        assert checkout_solution.checkout('D') == 15
+        assert checkout_solution.checkout('') == 0
