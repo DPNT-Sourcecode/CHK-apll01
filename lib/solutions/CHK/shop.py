3251d7cc6df34c65e45fdf0b6e6c677ff8cd462d
sbx_user1051 <sbx_user1051@169.254.19.173>
2020-02-16 22:08:03 +0000

    Sun Feb 16 22:08:03 UTC 2020

--- lib/solutions/CHK/shop.py
+++ lib/solutions/CHK/shop.py
@@ -1,7 +1,11 @@
+from .offer import FreebieOffer
+from .offer import MultiOffer
+
 class Shop(object):
     def __init__(self):
         self.prices = dict()
-        self.offers = dict()
+        self.free_offers = []
+        self.multi_offers = []
 
     def set_price(self, sku, price):
         self.prices[sku] = price
@@ -12,9 +16,17 @@ class Shop(object):
         else:
             return -1
 
-    def add_offer(self, sku, quantity, price):
-        offer = (sku, quantity, price)
-        self.offers[sku] = offer
+    def add_offer(self, sku, quantity, price_or_freebie):
+        if type(price_or_freebie) == int:
+            offer = MultiOffer(sku, quantity, price_or_freebie)
+            self.multi_offers.append(offer)
+        elif type(price_or_freebie) == str:
+            offer = FreebieOffer(sku, quantity, price_or_freebie)
+            self.free_offers.append(offer)
 
-    def get_offers(self):
-        return self.offers
+ 
+    def get_free_offers(self):
+        return self.free_offers
+
+    def get_multi_offers(self):
+        return self.multi_offers
