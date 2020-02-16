8fc86ccf28fefc6185acdf77f0389283c34f30b2
sbx_user1051 <sbx_user1051@169.254.19.173>
2020-02-16 19:09:24 +0000

    Sun Feb 16 19:09:24 UTC 2020

---
+++ lib/solutions/CHK/checkout.py
@@ -0,0 +1,4 @@
+class Checkout(object):
+    def __init__(self, shop):
+        self.shop = shop
+        self.items = dict()
--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,5 +4,14 @@ from .shop import Shop
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    shop = Shop()
+    shop.set_price('A', 50)
+    shop.set_price('B', 30)
+    shop.set_price('C', 20)
+    shop.set_price('D', 15
+    shop.add_offer('A', 3, 130)
+    shop.add_offer('B', 2, 45)
+
+    checkout = Checkout(shop)
+
 
